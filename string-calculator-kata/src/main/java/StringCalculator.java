43376106db4e352c060ab5b6ab7ecaeec9463f1d
michael g <mickael.georges@gmail.com>
2016-11-07 20:09:42 -0500

    usecase 1.1

--- string-calculator-kata/src/main/java/StringCalculator.java
+++ string-calculator-kata/src/main/java/StringCalculator.java
@@ -2,6 +2,12 @@
 public class StringCalculator {
 
 	public int add(String numbers){
+		
+		//chaine vide -- useCase 1.1
+		if(numbers.isEmpty())
+			return 0;
+		
+		
 		return -1;
 	}
 	
--- string-calculator-kata/src/test/java/TestCalculator.java
+++ string-calculator-kata/src/test/java/TestCalculator.java
@@ -1,18 +1,42 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestCalculator{
 
+	private StringCalculator cal=null;
 	
 	
+	@Before
+	public void init(){
+		cal=new StringCalculator();
+	}
+		
 	
 	//chaine vide --> 0
 	@Test
 	public void emptyStringReturnNull(){
-		
-		StringCalculator cal=new StringCalculator();
 		assertEquals(0, cal.add(""));
 	}
+	
+	
+	
+	private int myRandomInteger()
+	{
+	    return (int) (Math.random() * 10);
+	}
+	
+	//chaine avec 1 seul nombre -->  retour du nombre
+	@Test
+	public void StringWithUniqueNumberReturn(){
+		
+		//entre 0 et 9
+		int entierRandom = myRandomInteger();
+		assertEquals(entierRandom, cal.add(String.valueOf(entierRandom)));
+	}
+	
+	
 	
 }
